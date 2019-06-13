b2494ad3964088d83d5a703eb683a75e4005e378
zankeii <42954203+zankeii@users.noreply.github.com>
2019-06-13 00:32:52 -0400

    installing plugin social sharing

--- newsapp/src/app/components/noticia/noticia.component.ts
+++ newsapp/src/app/components/noticia/noticia.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Article} from "../../interfaces/interfaces";
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { ActionSheetController } from '@ionic/angular';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @Component({
   selector: 'app-noticia',
@@ -9,7 +11,7 @@ import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 })
 export class NoticiaComponent implements OnInit {
 @Input() noticia: Article;
-  constructor(private iab: InAppBrowser) { }
+  constructor(private socialSharing: SocialSharing, private iab: InAppBrowser, private actionSheetController: ActionSheetController) { }
 
   ngOnInit() {}
 
@@ -17,5 +19,42 @@ openNews() {
    // console.log('News', this.noticia.url);
   const browser = this.iab.create(this.noticia.url, '_system');
 }
+
+  async openMenu() {
+      const actionSheet = await this.actionSheetController.create({
+        mode: 'md',
+        backdropDismiss : false,
+        buttons: [{
+          text: 'Share',
+          icon: 'share',
+          cssClass: 'action-dark',
+          handler: () => {
+            console.log('Share clicked');
+            this.socialSharing.share(
+                this.noticia.title,
+                this.noticia.source.name,
+                '',
+                this.noticia.url
+            );
+          }
+        }, {
+          text: 'Favorite',
+          icon: 'star',
+          cssClass: 'action-dark',
+          handler: () => {
+            console.log('Play clicked');
+          }
+        }, {
+          text: 'Cancel',
+          icon: 'close',
+          cssClass: 'action-dark',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }]
+      });
+      await actionSheet.present();
+    }
 
 }
